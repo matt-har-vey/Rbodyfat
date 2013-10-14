a694ce87d52159483b10a53b793a7fea9c07d082
Matt Harvey <matt@teamdawg.org>
2013-10-14 14:19:10 -0700

    Add diff / filtering function; renaming for cleanup

--- bodyfat.R
+++ bodyfat.R
@@ -6,8 +6,8 @@ library('stringr')
 
 
 
-lastDays <- function(days, bf = bodyfat, skip = 0) {
-  bf[bf$time >= now() - ddays(days + skip) & bf$time <= now() - ddays(skip),]
+lastDays <- function(days, dfFitbit = fitbit, skip = 0) {
+  dfFitbit[dfFitbit$time >= now() - ddays(days + skip) & dfFitbit$time <= now() - ddays(skip),]
 }
 
 pacificDate <- function(d) {
@@ -16,9 +16,9 @@ pacificDate <- function(d) {
       with_tz(d, "America/Los_Angeles"), "day"))
 }
 
-dayMeans <- function(bf = bodyfat) {
-  aggregate(bf[,c("weight", "fat_percent", "lean_mass", "fat_mass")],
-            by = list(date = pacificDate(bf$time)),
+dayMeans <- function(dfFitbit = fitbit) {
+  aggregate(dfFitbit[,c("weight", "fat_percent", "lean_mass", "fat_mass")],
+            by = list(date = pacificDate(dfFitbit$time)),
             mean)
 }
 
@@ -29,10 +29,16 @@ prettyHist <- function(x, ...) {
   h
 }
 
+maDiff <- function(x) {
+ filterCoeff <- 1/3*c(0,1,1,1,0,0,0,0,0)-1/8*c(0,1,1,1,1,1,1,1,1)
+ padded <- c(x, rep(x[length(x)], rep(x[1], 4), length(filterCoeff)))
+ filter(as.ts(padded), filterCoeff, method = "conv", sides = 2, circular = TRUE)[1:length(x)]
+}
+
 
 
 readData <- function() {
-  sqlData <- function(user = 1) {
+  fitbitDataFrame <- function(user = 1) {
     sqliteSelect <- function(db, query) {
       sqliteDbd <- dbDriver('SQLite')
       con <- dbConnect(sqliteDbd, dbname = db)
@@ -88,14 +94,15 @@ readData <- function() {
     mfp
   }
     
-  bodyfat <<- sqlData()
-  mfp <<- mfpDataFrame()
-  jdf <<- merge(mfp, dayMeans(bodyfat))
+  fitbit <<- fitbitDataFrame()
+  food <<- mfpDataFrame()
+  fitbitAndFood <<- merge(food, dayMeans(fitbit))
 }
 
 
 
-scaleDiffs <- function(df, lag = 3) {
+
+fitbitDiffs <- function(df, lag = 3) {
   firstFrame <- data.frame(df[1:(dim(df)[1] - lag),1])
   names(firstFrame) <- c(names(df)[1])
   diffFrame <- data.frame(diff(as.matrix(df[,c("weight", "fat_percent", "fat_mass", "lean_mass")]), lag = lag))
@@ -105,34 +112,34 @@ scaleDiffs <- function(df, lag = 3) {
   
 
 
-floorTimes <- function(times, days) {
-	length <- days * 3600 * 24
-	as.Date(as.POSIXct(length * (as.double(times) %/% length), origin = "1970-01-01"))
-}
-
-fatBox <- function(bf = bodyfat, days = 3) {
-	with(bf, boxplot(fat_percent ~ floorTimes(time, days)))
-	title('Body Fat %')
-}
-
-leanBox <- function(bf = bodyfat, days = 3) {
-	with(bf, boxplot(lean_mass ~ floorTimes(time, days)))
-	title('Lean Mass')
-}
-
-allPlots <- function(bf = bodyfat) {
+allPlots <- function(dfFitbit = fitbit) {
+  floorTimes <- function(times, days) {
+    length <- days * 3600 * 24
+    as.Date(as.POSIXct(length * (as.double(times) %/% length), origin = "1970-01-01"))
+  }
+  
+  fatBox <- function(dfFitbit = dfFitbit, days = 3) {
+    with(dfFitbit, boxplot(fat_percent ~ floorTimes(time, days)))
+    title('Body Fat %')
+  }
+  
+  leanBox <- function(dfFitbit = dfFitbit, days = 3) {
+    with(dfFitbit, boxplot(lean_mass ~ floorTimes(time, days)))
+    title('Lean Mass')
+  }
+  
 	split.screen(c(1,2))
 	screen(1)
-  fatBox(bf)
+  fatBox(dfFitbit)
 	screen(2)
-  leanBox(bf)
+  leanBox(dfFitbit)
   close.screen(all = TRUE)
 }
 
 
 
-plotDayOfWeek <- function(data = bodyfat, dependent = "fat_percent") {
-  with(data, boxplot(formula(sprintf("%s ~ wday(time)", dependent)),
+plotDayOfWeek <- function(dfFitbit = fitbit, dependent = "fat_percent") {
+  with(dfFitbit, boxplot(formula(sprintf("%s ~ wday(time)", dependent)),
                      ylab = 'Body Fat %', main = 'Body Composition by Day of Week', xaxt="n"))
   axis(1, at = seq(1,7),
        labels = c("Sun","Mon","Tue","Wed","Thurs","Fri","Sat"))
@@ -140,28 +147,28 @@ plotDayOfWeek <- function(data = bodyfat, dependent = "fat_percent") {
 
 ma <- function(ts, n = 5) { filter(ts, rep(1/n, n), sides = 1) }
 
-plotMovingAverage <- function(bf = dayMeans(bodyfat), n = 5) { plot(na.omit(ma(bf$fat_percent, n))) }
+plotMovingAverage <- function(dfFitbit = dayMeans(fitbit), n = 5) { plot(na.omit(ma(dfFitbit$fat_percent, n))) }
 
 toPerWeek <- function(model) {
   model$coefficients["time"] * 3600 * 24 * 7
 }
 
-poundsPerWeek <- function(bf = bodyfat) {
-	toPerWeek(with(bf, lm(weight ~ time)))
+poundsPerWeek <- function(dfFitbit = fitbit) {
+	toPerWeek(with(dfFitbit, lm(weight ~ time)))
 }
 
-meanNetCals <- function(bf = bodyfat) {
-  s <- pacificDate(min(bf$time))
-  e <- pacificDate(max(bf$time))
+meanNetCals <- function(dfFitbit = fitbit) {
+  s <- pacificDate(min(dfFitbit$time))
+  e <- pacificDate(max(dfFitbit$time))
   mean(mfp$net_calories[mfp$date >= s & mfp$date <= e])
 }
 
-targetCals <- function(bf = bodyfat, targetPounds = 0) {
-  meanNetCals(bf) - 500 * poundsPerWeek(bf) + 500 * targetPounds
+targetCals <- function(dfFitbit = fitbit, targetPounds = 0) {
+  meanNetCals(dfFitbit) - 500 * poundsPerWeek(dfFitbit) + 500 * targetPounds
 }
 
-percentT <- function(bf, m = 8) {
-  t.test(bf$fat_percent, alternative = "less", mu = m)
+percentT <- function(dfFitbit, m = 8) {
+  t.test(dfFitbit$fat_percent, alternative = "less", mu = m)
 }
 
 pVals <- function(lags = seq(2,12)) {
@@ -201,7 +208,7 @@ linRegPlot <- function(data, column) {
   abline(mdl$coefficients[1] - rse, mdl$coefficients[2], lty = 'dashed')
   
   screen(2)
-  hist <- hist(y, main = '', xlab = '', ylab = '')
+  hist <- prettyHist(y, xlab = '', ylab = '')
   
   close.screen(all = TRUE)
   
@@ -211,18 +218,18 @@ linRegPlot <- function(data, column) {
        "summary" = summary(y))
 }
 
-bfp <- function(days = 10) {
+lrpPercent <- function(days = 10) {
   linRegPlot(lastDays(days), "fat_percent")
 }
 
-lean <- function(days = 10) {
+lrpLean <- function(days = 10) {
   linRegPlot(lastDays(days), "lean_mass")
 }
 
-fat <- function(days = 10) {
+lrpFat <- function(days = 10) {
   linRegPlot(lastDays(days), "fat_mass")
 }
 
-all <- function(days = 10) {
-  list(bfp = bfp(days), lean = lean(days), fat = fat(days))
+lrpAll <- function(days = 10) {
+  list(percent = lrpPercent(days), lean = lrpLean(days), fat = lrpFat(days))
 }
